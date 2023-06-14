be69a38e2a5d4e12400b95bf88a97c11b4e55e46
DevelopingHippo <developinghippo@gmail.com>
2023-06-14 16:20:03 -0500

    Index Page

--- Website/app/server.js
+++ Website/app/server.js
@@ -1,12 +1,20 @@
 // Constants
 const express = require('express');
+
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.use(express.static('www'))
+const path = require("path");
+const router = express.Router();
 
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+"/html/index.html"));
+});
+
+app.use(express.static('www'))
+app.use('/', router)
 app.listen(PORT, HOST, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
 });
