ca7ad1f20001f7f676b14b12dfa9ade9a76b2080
DevelopingHippo <developinghippo@gmail.com>
2023-06-16 11:22:29 -0500

    Index Page

--- Website/node/app/modules_control.js
+++
@@ -1,9 +0,0 @@
-var cHead = document.getElementsByTagName(“head”);
-var hHead = cHead[0];
-function AddModule(mFileName)
-{
-    var sTag = document.createElement(“script”);
-    sTag.setAttribute(“src”, mFileName);
-    sTag.setAttribute(“type”, “text/javascript”);
-    hHead.appendChild(sTag);
-}
