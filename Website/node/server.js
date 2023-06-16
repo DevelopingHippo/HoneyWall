573283ea4b78db310c15725023081b549cfb87e7
DevelopingHippo <developinghippo@gmail.com>
2023-06-15 20:03:25 -0500

    Index Page

--- Website/node/server.js
+++ Website/node/server.js
@@ -1,17 +1,20 @@
-'use strict';
-
+// Constants
 const express = require('express');
 
-// Constants
-const PORT = 3000;
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const path = require("path");
+const router = express.Router();
+
+router.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+"/www/html/index.html"));
 });
 
+app.use(express.static('/app/www/'))
+app.use('/', router)
 app.listen(PORT, HOST, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
 });
