4458048e3296c83e2a74c1ac2a78d4c35d816633
DevelopingHippo <developinghippo@gmail.com>
2023-06-15 20:10:24 -0500

    Index Page

--- Website/node/server.js
+++ Website/node/server.js
@@ -10,10 +10,10 @@ const path = require("path");
 const router = express.Router();
 
 router.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+"index.html"));
+    res.sendFile(path.join(__dirname+"/www/html/index.html"));
 });
 
-app.use(express.static('/app/'))
+app.use(express.static('/app/www/'))
 app.use('/', router)
 app.listen(PORT, HOST, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
