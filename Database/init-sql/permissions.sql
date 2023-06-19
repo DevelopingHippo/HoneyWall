2f2f9c1c36a3c735e7e8eb7cc4349f8a48ae2e9f
DevelopingHippo <developinghippo@gmail.com>
2023-06-19 14:26:18 -0500

    Login Page

--- Database/init-sql/permissions.sql
+++ Database/init-sql/permissions.sql
@@ -1,8 +1,8 @@
 CREATE USER 'honey'@'app_honey' IDENTIFIED BY 'P@ssw0rd'; #Change this password to be randomly generated
-GRANT INSERT ON honeywall.* TO 'honey'@'app_honey';
+GRANT INSERT ON honeywall.* TO 'honey'@'%';
 
 CREATE USER 'web'@'web_honey' IDENTIFIED BY 'P@ssw0rd'; #Change this password to be randomly generated
 GRANT SELECT ON honeywall.* TO 'web'@'web_honey';
-GRANT CREATE,ALTER,INSERT,UPDATE,DELETE,SELECT on www.* TO 'web'@'web_honey';
+GRANT INSERT,UPDATE,DELETE,SELECT on www.* TO 'web'@'%';
 
 FLUSH PRIVILEGES;
