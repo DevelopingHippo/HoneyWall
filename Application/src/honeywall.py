541f88cecfdbf86b3fe178cf9e05f8f40d1ecbe2
DevelopingHippo <developinghippo@gmail.com>
2023-06-20 12:55:35 -0500

    Registration Page

--- Application/src/honeywall.py
+++ Application/src/honeywall.py
@@ -1,1 +1,56 @@
-print("hello world")
+import socket
+
+import mysql.connector
+from threading import Thread
+import time
+
+database = mysql.connector.connect(host="db_honey", database='honeywall', user='honeywall', password='P@ssw0rd')
+cur = database.cursor()
+data_id = 0
+HOST = "0.0.0.0"
+
+
+def main():
+    global data_id
+    print("Starting HoneyWall Application")
+    cur.execute("SELECT max(id) as last_id FROM connections LIMIT 1")
+    result = cur.fetchone()
+    data_id = int(result) + 1
+    honeywall()
+
+
+def honeywall():
+    http_thread = Thread(target=http_socket)
+    https_thread = Thread(target=https_socket)
+    telnet_thread = Thread(target=telnet_socket())
+
+
+def http_socket():
+    print("Testing HTTP Thread")
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #     s.bind((HOST, 80))
+    #     s.listen()
+    #     client, addr = s.accept()
+    #     with client:
+    #         print("SRC IP: " + addr + "\n")
+    #         print("DST IP: ")
+
+def https_socket():
+    print("Testing HTTPS Thread")
+
+
+def telnet_socket():
+    print("Testing Telnet Thread")
+
+
+def query_connection(dst_ip, dst_port, src_ip, src_port, protocol, time, date, service, location, packets):
+    global data_id
+    query = "INSERT INTO connections VALUES (%d, %s, %d, %s, %d, %s, %s, %s, %s, %s, %d)"
+    data = (data_id, dst_ip, dst_port, src_ip, src_port, protocol, time, date, service, location, packets)
+    cur.execute(query, data)
+    database.commit()
+    data_id += 1
+
+
+if __name__ == "__main__":
+    main()
--- Database/init-sql/permissions.sql
+++ Database/init-sql/permissions.sql
@@ -1,5 +1,5 @@
 CREATE USER 'honey'@'%' IDENTIFIED BY 'P@ssw0rd'; #Change this password to be randomly generated
-GRANT INSERT ON honeywall.* TO 'honey'@'%';
+GRANT INSERT, SELECT ON honeywall.* TO 'honey'@'%';
 
 CREATE USER 'web'@'%' IDENTIFIED BY 'P@ssw0rd'; #Change this password to be randomly generated
 GRANT SELECT ON honeywall.* TO 'web'@'%';
