2643a85079a2bacad14039d0d9c658699b093332
DevelopingHippo <developinghippo@gmail.com>
2023-06-20 11:20:38 -0500

    Registration Page

--- Website/node/app/js/database_functions.js
+++ Website/node/app/js/database_functions.js
@@ -1,18 +1,21 @@
 const mysql = require('mysql');
-
-alert("The database page loaded")
-
-var con = mysql.createConnection({
+const con = mysql.createConnection({
     host: "db_honey",
     user: "web",
     password: "P@ssw0rd",
     database: "www"
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM users", function (err, result, fields) {
+function queryDatabase() {
+    alert("The database page loaded")
+    con.connect(function(err) {
         if (err) throw err;
-        console.log(result);
+        con.query("SELECT * FROM users", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
     });
-});
+}
+
+
+
