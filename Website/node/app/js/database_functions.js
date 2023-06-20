a7897597532fb9068377870ac3a4c53c76b50360
DevelopingHippo <developinghippo@gmail.com>
2023-06-20 11:14:41 -0500

    Registration Page

--- Website/node/app/js/database_functions.js
+++ Website/node/app/js/database_functions.js
@@ -6,13 +6,13 @@ var con = mysql.createConnection({
     host: "db_honey",
     user: "web",
     password: "P@ssw0rd",
-    database: "honeywall"
+    database: "www"
 });
 
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM connections", function (err, result, fields) {
-        if (err) console.log(err);
+    con.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
         console.log(result);
     });
 });
