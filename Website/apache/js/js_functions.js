779059a7314e263865a22967b1c391464f183297
DevelopingHippo <developinghippo@gmail.com>
2023-06-20 11:56:57 -0500

    Registration Page

--- Website/apache/js/js_functions.js
+++ Website/apache/js/js_functions.js
@@ -1,7 +1,5 @@
 
 function changeColor() {
     // Generating random color each time
-    let color = "#" + (Math.random() * 16777215 | 0).toString(16);
-
-    $("body").css("background-color", color);
+    document.body.style.background = "#" + (Math.random() * 16777215 | 0).toString(16);
 }
