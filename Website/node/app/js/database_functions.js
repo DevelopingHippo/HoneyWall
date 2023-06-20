4a59fb5b6442f1bb97d3b60c73c4189e49b6adc3
DevelopingHippo <developinghippo@gmail.com>
2023-06-20 11:26:49 -0500

    Registration Page

--- Website/node/app/js/database_functions.js
+++ Website/node/app/js/database_functions.js
@@ -1,12 +1,13 @@
-let mysql = require('mysql');
-let con = mysql.createConnection({
-    host: "db_honey",
-    user: "web",
-    password: "P@ssw0rd",
-    database: "www"
-});
+import mysql from "mysql";
+
 
 function queryDatabase() {
+    const con = mysql.createConnection({
+        host: "db_honey",
+        user: "web",
+        password: "P@ssw0rd",
+        database: "www"
+    });
     alert("The database page loaded")
     con.connect(function(err) {
         if (err) throw err;
