7627ef17935a3ab36765961d29e1cafc20d2bf6e
DevelopingHippo <developinghippo@gmail.com>
2023-06-22 14:51:43 -0500

    Dashboard CSS

--- Website/apache/dashboard/dashboard.php
+++ Website/apache/dashboard/dashboard.php
@@ -1,33 +1,32 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Dashboard</title>
-  <script src="../js/js_functions.js"></script>
-  <script src="../js/d3.v7.js"></script>
-  <!--<script src="https://d3js.org/d3.v7.min.js"></script>-->
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="dashboard.css">
+    <script src="../js/js_functions.js"></script>
+    <script src="../js/d3.v7.js"></script>
+    <!--<script src="https://d3js.org/d3.v7.min.js"></script>-->
 </head>
+    <h1><b>Dashboard</b></h1>
 <body>
-<h1><b>Dashboard</b></h1>
-<button type="button" onclick="changeColor()">Try it</button>
-
-<script>
-var data = [2, 4, 8, 10]
-var pie = d3.pie()
-console.log(pie(data))
-</script>
-
-
-
-
-
-<?php
-require_once "../php/database_functions.php";
-$result = queryDatabase_honeywall("SELECT * FROM connections");
-while($row = mysqli_fetch_array($result))
-{
-    print_r($row);
-}
-?>
+    <section>
+        <div class="stats">
+            <div class="pie-charts-grid">
+                <div class="top-geolocation-pie">Top Geolocations</div>
+                <div class="top-ports-pie">Top Ports</div>
+                <div class="top-usernames-pie">Top Usernames</div>
+                <div class="top-passwords-pie">Top Passwords</div>
+            </div>
+            <div class="stat-map">
+                <div class="live-map">Map</div>
+                <div class="vertical-stats">
+                    <div class="top-geolocation-vert">Top Geolocation</div>
+                    <div class="top-ports-vert">Top Ports</div>
+                    <div class="top-services-vert">Top Services</div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
+
 </html>
