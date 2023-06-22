c68f5921e8af52e7bccfd470e2da1e520ca750ee
DevelopingHippo <developinghippo@gmail.com>
2023-06-22 15:17:47 -0500

    Dashboard CSS

--- Website/apache/dashboard/dashboard.php
+++ Website/apache/dashboard/dashboard.php
@@ -7,26 +7,26 @@
     <script src="../js/d3.v7.js"></script>
     <!--<script src="https://d3js.org/d3.v7.min.js"></script>-->
 </head>
-    <h1><b>Dashboard</b></h1>
+<h1><b>Dashboard</b></h1>
 <body>
-    <section>
-        <div class="stats">
-            <div class="pie-charts-grid">
-                <div class="top-geolocation-pie">Top Geolocations</div>
-                <div class="top-ports-pie">Top Ports</div>
-                <div class="top-usernames-pie">Top Usernames</div>
-                <div class="top-passwords-pie">Top Passwords</div>
-            </div>
-            <div class="stat-map">
-                <div class="live-map">Map</div>
-                <div class="vertical-stats">
-                    <div class="top-geolocation-vert">Top Geolocation</div>
-                    <div class="top-ports-vert">Top Ports</div>
-                    <div class="top-services-vert">Top Services</div>
-                </div>
+<section>
+    <div class="stats">
+        <div class="pie-charts-grid">
+            <div class="top-geolocation-pie">Top Geolocations</div>
+            <div class="top-ports-pie">Top Ports</div>
+            <div class="top-usernames-pie">Top Usernames</div>
+            <div class="top-passwords-pie">Top Passwords</div>
+        </div>
+        <div class="stat-map">
+            <div class="live-map">Map</div>
+            <div class="vertical-stats">
+                <div class="top-geolocation-vert">Top Geolocation</div>
+                <div class="top-ports-vert">Top Ports</div>
+                <div class="top-services-vert">Top Services</div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 </body>
 
 </html>
