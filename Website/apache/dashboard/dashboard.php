561a7d2e8153b1a9a9d64f94ab9e4b2029dee63b
DevelopingHippo <developinghippo@gmail.com>
2023-06-22 11:29:26 -0500

    HoneyWall Application

--- Website/apache/dashboard/dashboard.php
+++ Website/apache/dashboard/dashboard.php
@@ -4,11 +4,22 @@
   <meta charset="UTF-8">
   <title>Dashboard</title>
   <script src="../js/js_functions.js"></script>
-    <script>src="../js/d3.v7.min.js"</script>
+  <script src="../js/d3.v7.js"></script>
+  <script src="https://d3js.org/d3.v7.min.js"></script>
 </head>
 <body>
 <h1><b>Dashboard</b></h1>
 <button type="button" onclick="changeColor()">Try it</button>
+
+<script>
+var data = [2, 4, 8, 10]
+var pie = d3.pie()
+console.log(pie(data))
+</script>
+
+
+
+
 
 <?php
 require_once "../php/database_functions.php";
