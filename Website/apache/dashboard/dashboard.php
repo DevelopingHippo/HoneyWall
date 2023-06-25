ecb2e6c1d6a2e3ed6283216675f950da22ae9a52
DevelopingHippo <developinghippo@gmail.com>
2023-06-24 23:30:31 -0500

    Dashboard CSS

--- Website/apache/dashboard/dashboard.php
+++ Website/apache/dashboard/dashboard.php
@@ -1,38 +1,47 @@
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
     <link rel="stylesheet" href="dashboard.css">
+    <link rel="stylesheet" href="../js/jvectormap/jquery-jvectormap-2.0.5.css" type="text/css" media="screen"/>
     <script src="../js/js_functions.js"></script>
     <script src="../js/d3.v7.js"></script>
-    <link rel="stylesheet" href="../js/jvectormap/jquery-jvectormap-2.0.5.css" type="text/css" media="screen"/>
     <script src="../js/jvectormap/jquery-3.7.0.js"></script>
     <script src="../js/jvectormap/jquery-jvectormap-2.0.5.min.js"></script>
     <script src="../js/jvectormap/jvectormap-world-mill.js"></script>
-    <script src="packetData.js"
-    <!--<script src="https://d3js.org/d3.v7.min.js"></script>-->
+    <script src="packetData.js"></script>
 </head>
-<h1><b>Dashboard</b></h1>
+<header>
+
+</header>
 <body>
 <section>
     <div class="stats">
 
-        <div class="top-geolocation-pie">Top Geolocations</div>
-        <div class="top-ports-pie">Top Ports</div>
-        <div class="top-usernames-pie">Top Usernames</div>
-        <div class="top-passwords-pie">Top Passwords</div>
+        <div class="top-geolocation-pie"><h4>Top Geolocations</h4>
+            <svg id="geolocation-svg" width="300" height="300"></svg>
+            <script>drawPieChart("#geolocation-svg")</script>
+        </div>
+
+        <div class="top-ports-pie"><h4>Top Ports</h4>
+            <svg id="top-ports-svg" width="300" height="300"></svg>
+            <script>drawPieChart("#top-ports-svg")</script></div>
+
+        <div class="top-usernames-pie"><h4>Top Usernames</h4>
+            <svg id="top-usernames-svg" width="300" height="300"></svg>
+            <script>drawPieChart("#top-usernames-svg")</script></div>
+
+        <div class="top-passwords-pie"><h4>Top Passwords</h4>
+            <svg id="top-passwords-svg" width="300" height="300"></svg>
+            <script>drawPieChart("#top-passwords-svg")</script></div>
 
         <div class="live-map">Map
             <div id="world-map" style="width: 600px; height: 400px;margin: auto"></div>
-            <!--
             <script>
-                const element = document.getElementById("world-map");
-                element.addEventListener('contextmenu', event => event.preventDefault())
-                $(function(){
-                    $('#world-map').vectorMap({map: 'world_mill'});
+                document.getElementById("world-map").addEventListener("contextmenu", (event) => {
+                    event.preventDefault();
                 });
             </script>
-            -->
         <script>
             $('#world-map').vectorMap({
                 map: 'world_mill',
@@ -59,5 +68,7 @@
     </div>
 </section>
 </body>
+<footer>
 
+</footer>
 </html>
--- Website/apache/js/js_functions.js
+++ Website/apache/js/js_functions.js
@@ -3,3 +3,57 @@ function changeColor() {
     // Generating random color each time
     document.body.style.background = "#" + (Math.random() * 16777215 | 0).toString(16);
 }
+
+function drawPieChart(chartType)
+{
+    var svg = d3.select(chartType),
+        width = svg.attr("width"),
+        height = svg.attr("height"),
+        radius = width / 2;
+
+    // Step 1
+    var data = [{name: "Alex", share: 20.70},
+        {name: "Shelly", share: 30.92},
+        {name: "Clark", share: 15.42},
+        {name: "Matt", share: 13.65},
+        {name: "Jolene", share: 19.31}];
+
+    var g = svg.append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    // Step 4
+    var ordScale = d3.scaleOrdinal()
+        .domain(data)
+        .range(['#ffd384','#94ebcd','#fbaccc','#d3e0ea','#fa7f72']);
+
+    // Step 5
+    var pie = d3.pie().value(function(d) {
+        return d.share;
+    });
+
+    var arc = g.selectAll("arc")
+        .data(pie(data))
+        .enter();
+
+    // Step 6
+    var path = d3.arc()
+        .outerRadius(radius)
+        .innerRadius(0);
+
+    arc.append("path")
+        .attr("d", path)
+        .attr("fill", function(d) { return ordScale(d.data.name); });
+
+    // Step 7
+    var label = d3.arc()
+        .outerRadius(radius)
+        .innerRadius(0);
+
+    arc.append("text")
+        .attr("transform", function(d) {
+            return "translate(" + label.centroid(d) + ")";
+        })
+        .text(function(d) { return d.data.name; })
+        .style("font-family", "arial")
+        .style("font-size", 15);
+}
