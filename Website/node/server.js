0595a94f9c02e04cc417c7ae2d5845ad9c6bc3ec
DevelopingHippo <developinghippo@gmail.com>
2023-06-29 18:05:11 -0500

    API Vertical Request from SQL Database (Working)

--- Website/node/server.js
+++ Website/node/server.js
@@ -119,34 +119,6 @@ app.get('/get-chart-data', cors(corsOptions), function(req, res){
     );
 });
 
-app.get('/get-port-data', cors(corsOptions), function(req, res){
-
-    res.setHeader('Content-Type', 'application/json');
-    let query = "select dst_port as port,COUNT(*) as total_count from connections GROUP BY dst_port ORDER BY total_count DESC LIMIT 8;";
-
-    const con = mysql.createConnection({
-        host: "db_honey",
-        user: "web",
-        password: "P@ssw0rd",
-        database: "honeywall"
-    });
-    con.connect(function(err) {
-        if (err) throw err;
-        con.query(query, function (err, result) {
-            let formatted_result = '{';
-            if (err) throw err;
-            else {
-                for (var i = 0; i < result.length - 1; i++) {
-                    formatted_result += '"' + result[i]['port'] + '":' + result[i]['total_count'] + ',';
-                }
-                formatted_result += '"' + result[result.length - 1]['port'] + '":' + result[result.length - 1]['total_count'] + "}";
-                let json_format = JSON.parse(formatted_result);
-                res.json(json_format);
-                con.end();
-            }
-        });
-    });
-});
 
 app.get('/get-vert-data', cors(corsOptions), function(req, res){
 
@@ -185,7 +157,7 @@ app.get('/get-vert-data', cors(corsOptions), function(req, res){
                 for (var i = 0; i < result.length - 1; i++) {
                     formatted_result += '"' + result[i]['data'] + '":' + result[i]['total_count'] + ',';
                 }
-                formatted_result += '"' + result[result.length - 1]['port'] + '":' + result[result.length - 1]['total_count'] + "}";
+                formatted_result += '"' + result[result.length - 1]['data'] + '":' + result[result.length - 1]['total_count'] + "}";
                 let json_format = JSON.parse(formatted_result);
                 res.json(json_format);
                 con.end();
