46c225d17ccc8720799bc8dda9f464d36ecf4d01
DevelopingHippo <developinghippo@gmail.com>
2023-07-26 15:01:32 -0500

    Optimizing API connections

--- Website/node/server.js
+++ Website/node/server.js
@@ -88,119 +88,128 @@ app.get('/get-vert-data', cors(corsOptions), async function (req, res) {
 });
 
 
-    app.get('/get-pie-data', cors(corsOptions), async function (req, res) {
+app.get('/get-pie-data', cors(corsOptions), async function (req, res) {
 
-        try {
-            res.setHeader('Content-Type', 'application/json');
-            let query;
-
-            if (req.query['type'] === "geo") {
-                query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
-            } else if (req.query['type'] === "ports") {
-                query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
-            } else if (req.query['type'] === "services") {
-                query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
-            } else if (req.query['type'] === "ip") {
-                query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
-            } else if (req.query['type'] === "username") {
-                query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
-            } else if (req.query['type'] === "password") {
-                query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
-            }
-            let total = 0.00;
-            const result = await db_query(query);
-            let formatted_result = '[';
-            for (let i = 0; i < result.length; i++) {
-                total = total + parseFloat(result[i]['total_count']);
-            }
-            for (let i = 0; i < result.length - 1; i++) {
-                formatted_result += '{"name": "' + result[i]['data'] + '", "share": ' + ((parseFloat(result[i]['total_count']) / total) * 100).toFixed(2) + '},';
-            }
-            formatted_result += '{"name": "' + result[result.length - 1]['data'] + '", "share": ' + ((parseFloat(result[result.length - 1]['total_count']) / total) * 100).toFixed(2) + "}]";
-            let json_format = JSON.parse(formatted_result);
-            res.json(json_format);
-        }
-        catch (error){
-            console.error('Error executing the query:', error);
-            res.status(500).json({ error: 'Internal Server Error' });
-        }
-    });
-
-
-    app.get('/get-chart-data', cors(corsOptions), function (req, res) {
+    try {
         res.setHeader('Content-Type', 'application/json');
+        let query;
 
-        res.json([
-                {
-                    "month": "January",
-                    "packet_number": 1000
-                },
-                {
-                    "month": "February",
-                    "packet_number": 1200
-                },
-                {
-                    "month": "March",
-                    "packet_number": 300
-                },
-                {
-                    "month": "April",
-                    "packet_number": 400
-                },
-                {
-                    "month": "May",
-                    "packet_number": 450
-                },
-                {
-                    "month": "June",
-                    "packet_number": 355
-                },
-                {
-                    "month": "July",
-                    "packet_number": 203
-                },
-                {
-                    "month": "August",
-                    "packet_number": 122
-                },
-                {
-                    "month": "September",
-                    "packet_number": 700
-                },
-                {
-                    "month": "October",
-                    "packet_number": 960
-                },
-                {
-                    "month": "November",
-                    "packet_number": 104
-                },
-                {
-                    "month": "December",
-                    "packet_number": 1010
-                }
-            ]
-        );
-    });
+        if (req.query['type'] === "geo") {
+            query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
+        } else if (req.query['type'] === "ports") {
+            query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
+        } else if (req.query['type'] === "services") {
+            query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
+        } else if (req.query['type'] === "ip") {
+            query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
+        } else if (req.query['type'] === "username") {
+            query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
+        } else if (req.query['type'] === "password") {
+            query = "select location as data, count(*) as total_count FROM connections GROUP BY data ORDER BY total_count DESC LIMIT 5;";
+        }
+        let total = 0.00;
+        const result = await db_query(query);
+        let formatted_result = '[';
+        for (let i = 0; i < result.length; i++) {
+            total = total + parseFloat(result[i]['total_count']);
+        }
+        for (let i = 0; i < result.length - 1; i++) {
+            formatted_result += '{"name": "' + result[i]['data'] + '", "share": ' + ((parseFloat(result[i]['total_count']) / total) * 100).toFixed(2) + '},';
+        }
+        formatted_result += '{"name": "' + result[result.length - 1]['data'] + '", "share": ' + ((parseFloat(result[result.length - 1]['total_count']) / total) * 100).toFixed(2) + "}]";
+        let json_format = JSON.parse(formatted_result);
+        res.json(json_format);
+    }
+    catch (error){
+        console.error('Error executing the query:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+app.get('/get-chart-data', cors(corsOptions), function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+
+    res.json([
+            {
+                "month": "January",
+                "packet_number": 1000
+            },
+            {
+                "month": "February",
+                "packet_number": 1200
+            },
+            {
+                "month": "March",
+                "packet_number": 300
+            },
+            {
+                "month": "April",
+                "packet_number": 400
+            },
+            {
+                "month": "May",
+                "packet_number": 450
+            },
+            {
+                "month": "June",
+                "packet_number": 355
+            },
+            {
+                "month": "July",
+                "packet_number": 203
+            },
+            {
+                "month": "August",
+                "packet_number": 122
+            },
+            {
+                "month": "September",
+                "packet_number": 700
+            },
+            {
+                "month": "October",
+                "packet_number": 960
+            },
+            {
+                "month": "November",
+                "packet_number": 104
+            },
+            {
+                "month": "December",
+                "packet_number": 1010
+            }
+        ]
+    );
+});
 
 // Function to execute database queries
-    function db_query(sql, params) {
-        return new Promise((resolve, reject) => {
-            db_pool.query(sql, params, (error, results) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve(results);
-                }
-            });
+function db_query(sql, params) {
+    return new Promise((resolve, reject) => {
+        db_pool.query(sql, params, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
         });
-    }
+    });
+}
+
+// Function to close the database connection (optional)
+function closeConnection() {
+    db_pool.end((err) => {
+        if (err) {
+            console.error('Error closing the database connection:', err);
+        } else {
+            console.log('Database connection closed successfully.');
+        }
+    });
+}
 
 
 // const server = http2.createSecureServer(options, app)
 // server.listen(PORT)
-    app.listen(PORT, HOST, () => {
-        console.log(`Running on http://${HOST}:${PORT}`);
-    });
-)
-}
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
