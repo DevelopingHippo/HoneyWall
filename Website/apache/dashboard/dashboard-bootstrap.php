3fa5684e65d88c18ba880aae25880ce7b8a2b500
Thad Sander <developinghippo@gmail.com>
2023-10-06 13:27:48 -0500

    Bootstrap Pies

--- Website/apache/dashboard/dashboard-bootstrap.php
+++ Website/apache/dashboard/dashboard-bootstrap.php
@@ -17,20 +17,18 @@ loginCheck();
     <link rel="stylesheet" href="../css/site-global.css">
     <script src="build_stats-bootstrap.js"></script>
 
-    <!-- Unknown?
-    <script src="../js/d3.v7.js"></script> -->
-
     <!-- Pie Graph Dependencies -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
     <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js'></script>
 
-    <!-- Map Dependencies -->
+    <!-- Map & Bar Graph Dependencies -->
     <link rel="stylesheet" href="../js/jvectormap/jquery-jvectormap-2.0.5.css" type="text/css" media="screen"/>
     <script src="../js/jvectormap/jquery-jvectormap-2.0.5.min.js"></script>
     <script src="../js/jvectormap/jvectormap-world-mill.js"></script>
     <script src="../js/jvectormap/jquery-3.7.0.js"></script>
+    <script src="../js/d3.v7.js"></script>
 
 </head>
 <body>
