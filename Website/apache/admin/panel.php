879c1ffa287c6cd80a5d0a1d56f005b4bd64e1b3
Thad Sander <developinghippo@gmail.com>
2023-10-06 12:22:05 -0500

    Fixing Website

--- Website/apache/admin/panel.php
+++ Website/apache/admin/panel.php
@@ -5,3 +5,22 @@ if(!isset($_SESSION["admin"]) || $_SESSION["admin"] != "true")
     header("location: /auth/login.php");
     exit();
 }
+?>
+<!DOCTYPE html>
+<?php
+require_once "../php/web_functions.php";
+loginCheck();
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="panel.css">
+    <link rel="stylesheet" href="../css/site-global.css">
+</head>
+<body>
+</body>
+<?php
+printFooter();
+?>
+</html>
