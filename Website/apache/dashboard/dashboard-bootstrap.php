2f80a093e1daa53c830dd99f593b22f75a60b35d
Thad Sander <developinghippo@gmail.com>
2023-10-06 13:02:05 -0500

    Bootstrap Pies

--- Website/apache/dashboard/dashboard-bootstrap.php
+++ Website/apache/dashboard/dashboard-bootstrap.php
@@ -15,6 +15,10 @@ loginCheck();
     <link rel="stylesheet" href="../css/site-global.css">
     <link rel="stylesheet" href="../js/jvectormap/jquery-jvectormap-2.0.5.css" type="text/css" media="screen"/>
     <script src="../js/d3.v7.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js'></script>
     <script src="../js/jvectormap/jquery-3.7.0.js"></script>
     <script src="../js/jvectormap/jquery-jvectormap-2.0.5.min.js"></script>
     <script src="../js/jvectormap/jvectormap-world-mill.js"></script>
