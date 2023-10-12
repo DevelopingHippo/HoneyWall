2f4aa500bc5987bb4e5e26d4277235efbde41e69
DevelopingHippo <developinghippo@gmail.com>
2023-10-12 11:41:04 -0400

    Designing Pies (Also rewrote the building the pies)

--- Website/apache/dashboard/dashboard-bootstrap.php
+++ Website/apache/dashboard/dashboard-bootstrap.php
@@ -96,5 +96,30 @@ printPreloader();
 <?php
 printFooter();
 ?>
+
+<script>
+    $(document).ready(function() {
+        var ctx = $("#chart-line");
+        var myLineChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ["Spring", "Summer", "Fall", "Winter"],
+                datasets: [{
+                    data: [1200, 1700, 800, 200],
+                    backgroundColor: ["rgba(255, 0, 0, 0.5)", "rgba(100, 255, 0, 0.5)", "rgba(200, 50, 255, 0.5)", "rgba(0, 100, 255, 0.5)"]
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Weather'
+                }
+            }
+        });
+    });
+</script>
+
+<canvas id="chart-line" width="300" height="200" class="chartjs-render-monitor" style="display: block; width: 299px; height: 200px;"></canvas>
+
 </body>
 </html>
