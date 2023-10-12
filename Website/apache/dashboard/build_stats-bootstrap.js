3063084b95446bd5170959483814e33c7f7d60c6
DevelopingHippo <developinghippo@gmail.com>
2023-10-12 11:43:07 -0400

    Designing Pies (Also rewrote the building the pies)

--- Website/apache/dashboard/build_stats-bootstrap.js
+++ Website/apache/dashboard/build_stats-bootstrap.js
@@ -164,15 +164,21 @@ async function build_pie(type, position){
     const data_array = Object.values(data);
 
     $(document).ready(function() {
-        var ctx = "#chart-pie-" + position;
+        var ctx = $("#chart-line");
         var myLineChart = new Chart(ctx, {
             type: 'pie',
             data: {
                 labels: ["Spring", "Summer", "Fall", "Winter"],
                 datasets: [{
                     data: [1200, 1700, 800, 200],
-                    backgroundColor: ["rgba(255, 0, 0, 0.5)", "rgba(100, 255, 0, 0.5)", "rgba(200, 50, 255, 0.5)", "rgba(0, 100, 255, 0.5)", "rgba(0, 255, 255, 0.5)"]
+                    backgroundColor: ["rgba(255, 0, 0, 0.5)", "rgba(100, 255, 0, 0.5)", "rgba(200, 50, 255, 0.5)", "rgba(0, 100, 255, 0.5)"]
                 }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'Weather'
+                }
             }
         });
     });
