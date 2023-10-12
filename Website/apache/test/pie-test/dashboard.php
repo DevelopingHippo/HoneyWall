6d315831ae255ec86c4c9541b1a043145cdbab03
DevelopingHippo <developinghippo@gmail.com>
2023-10-12 11:53:56 -0400

    Designing Pies (Also rewrote the building the pies)

--- Website/apache/test/pie-test/dashboard.php
+++ Website/apache/test/pie-test/dashboard.php
@@ -7,15 +7,16 @@ require_once "../../php/web_functions.php";
     <meta charset="UTF-8">
     <title>Dashboard</title>
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="dashboard.css" type="text/css">
+    <link rel="stylesheet" href="../../css/site-global.css" type="text/css">
+
     <!-- Pie Graph Dependencies -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" type="text/css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js'></script>
 
     <!-- Internal Dependencies -->
-    <link rel="stylesheet" href="dashboard.css" type="text/css">
-    <link rel="stylesheet" href="../../css/site-global.css" type="text/css">
     <script src="build_stats-bootstrap.js"></script>
 </head>
 <body>
@@ -42,16 +43,10 @@ printPreloader();
         </div>
 
         <script>
-            build_bar_chart();
-            build_map();
             build_pie("location", 1);
             build_pie("dst_port", 2)
             build_pie("src_ip", 3);
             build_pie("service", 4);
-            build_vert("location", 1);
-            build_vert("dst_port", 2);
-            build_vert("service", 3);
-            build_vert("src_ip", 4);
         </script>
     </div>
 </section>
