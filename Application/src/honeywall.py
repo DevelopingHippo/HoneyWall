f7676b71c9f60447b29915fea8b9ee2df03880e3
DevelopingHippo <developinghippo@gmail.com>
2023-10-12 19:17:02 -0400

    Designing Pies (Also rewrote the building the pies)

--- Application/src/honeywall.py
+++ Application/src/honeywall.py
@@ -22,38 +22,6 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 
-#read the file, send contents to the database connection method, clear the file
-def ssh_log_push():
-  ssh_log = open(ssh_logpath, 'w')
-  for line in ssh_log:
-    query_connection(line)
-
-  ssh_log.close()
-
-def http_log_push():
-  http_log = open(http_logpath, 'w')
-
-  http_log.close()
-
-def https_log_push():
-  https_log = open(https_logpath, 'w')
-
-  https_log.close()
-
-def telnet_log_push():
-  telnet_log = open(telnet_logpath, 'w')
-
-  telnet_log.close()
-
-
-#connection to the database to actually push the data
-def query_connection(dst_ip, dst_port, src_ip, src_port, protocol, time_of_attack, date, service, location, packets):
-  query = "INSERT INTO connections VALUES (%d, %s, %d, %s, %d, %s, %s, %s, %s, %s, %d)"
-  data = (dst_ip, dst_port, src_ip, src_port, protocol, time_of_attack, date, service, location, packets)
-  cursor.execute(query, data)
-  db.commit()
-
-
 #while loop to constantly run through each protocol push and wait 30 seconds
 while True:
   ssh_log_push()
@@ -61,3 +29,34 @@ while True:
   #https_log_push()
   #telnet_log_push()
   time.sleep(30)
+
+#read the file, send contents to the database connection method, clear the file
+def ssh_log_push():
+ssh_log = open(ssh_logpath, 'w')
+for line in ssh_log:
+  query_connection(line)
+
+ssh_log.close()
+
+def http_log_push():
+http_log = open(http_logpath, 'w')
+
+http_log.close()
+
+def https_log_push():
+https_log = open(https_logpath, 'w')
+
+https_log.close()
+
+def telnet_log_push():
+telnet_log = open(telnet_logpath, 'w')
+
+telnet_log.close()
+
+
+#connection to the database to actually push the data
+def query_connection(dst_ip, dst_port, src_ip, src_port, protocol, time_of_attack, date, service, location, packets):
+query = "INSERT INTO connections VALUES (%d, %s, %d, %s, %d, %s, %s, %s, %s, %s, %d)"
+data = (dst_ip, dst_port, src_ip, src_port, protocol, time_of_attack, date, service, location, packets)
+cursor.execute(query, data)
+db.commit()
