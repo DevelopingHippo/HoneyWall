beba07df6f8c8258333a41532dfcffd819835391
DevelopingHippo <developinghippo@gmail.com>
2023-10-12 11:36:38 -0400

    Designing Pies (Also rewrote the building the pies)

--- Website/apache/dashboard/dashboard-bootstrap.php
+++ Website/apache/dashboard/dashboard-bootstrap.php
@@ -12,23 +12,23 @@ loginCheck();
     <meta charset="UTF-8">
     <title>Dashboard</title>
 
+    <!-- Pie Graph Dependencies -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" type="text/css">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js'></script>
+
+    <!-- Map & Bar Graph Dependencies -->
+    <script src="../js/jvectormap/jquery-3.7.0.js"></script>
+    <link rel="stylesheet" href="../js/jvectormap/jquery-jvectormap-2.0.5.css" type="text/css" media="screen"/>
+    <script src="../js/jvectormap/jquery-jvectormap-2.0.5.min.js"></script>
+    <script src="../js/jvectormap/jvectormap-world-mill.js"></script>
+    <script src="../js/d3.v7.js"></script>
+
     <!-- Internal Dependencies -->
     <link rel="stylesheet" href="dashboard.css" type="text/css">
     <link rel="stylesheet" href="../css/site-global.css" type="text/css">
     <script src="build_stats-bootstrap.js"></script>
-
-    <!-- Pie Graph Dependencies -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" type="text/css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.bundle.min.js'></script>
-
-    <!-- Map & Bar Graph Dependencies -->
-    <link rel="stylesheet" href="../js/jvectormap/jquery-jvectormap-2.0.5.css" type="text/css" media="screen"/>
-    <script src="../js/jvectormap/jquery-jvectormap-2.0.5.min.js"></script>
-    <script src="../js/jvectormap/jvectormap-world-mill.js"></script>
-    <script src="../js/jvectormap/jquery-3.7.0.js"></script>
-    <script src="../js/d3.v7.js"></script>
 
 </head>
 <body>
