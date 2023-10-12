53ef440c63a60652a63e23800be5de83cfea72e2
DevelopingHippo <developinghippo@gmail.com>
2023-10-12 19:06:53 -0400

    Designing Pies (Also rewrote the building the pies)

--- Application/src/honeywall.py
+++ Application/src/honeywall.py
@@ -1,16 +1,65 @@
 #imports for the program
 import os
 import mysql.connector
+import time
 
-#setting variables
-filepath = "/var/logs/honeypots/"
+#setting log path variables
+ssh_logpath = "/var/logs/honeypots/ssh.log"
+http_logpath = "/var/logs/honeypots/http.log"
+https_logpath = "/var/logs/honeypots/https.log"
+telnet_logpath = "/var/logs/honeypots/telnet.log"
 
 #executing command to start the honeypots
 os.system("sudo -E python3 -m honeypots --setup ssh,http,https,telnet --config honeypotconfig.json")
 
 #connecting to db
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="username",
-  password="password"
+db = mysql.connector.connect(
+  host="db_honey",
+  user="honeywall",
+  password="P@ssw0rd",
+  database="honeywall"
 )
+cursor = db.cursor()
+
+
+#read the file, send contents to the database connection method, clear the file
+def ssh_log_push():
+  ssh_log = open(ssh_logpath, 'w')
+  for line in ssh_log:
+    query_connection(line)
+
+  ssh_log.close()
+
+def http_log_push():
+  http_log = open(http_logpath, 'w')
+
+  http_log.close()
+
+def https_log_push():
+  https_log = open(https_logpath, 'w')
+
+  https_log.close()
+
+def telnet_log_push():
+  telnet_log = open(telnet_logpath, 'w')
+
+  telnet_log.close()
+
+
+#connection to the database to actually push the data
+def query_connection(dst_ip, dst_port, src_ip, src_port, protocol, time_of_attack, date, service, location, packets):
+  global data_id
+  query = "INSERT INTO connections VALUES (%d, %s, %d, %s, %d, %s, %s, %s, %s, %s, %d)"
+  data = (data_id, dst_ip, dst_port, src_ip, src_port, protocol, time_of_attack, date, service, location, packets)
+  cursor.execute(query, data)
+  db.commit()
+  data_id += 1
+
+
+#while loop to constantly run through each protocol push and wait 30 seconds
+while True:
+  ssh_log_push()
+  #http_log_push()
+  #https_log_push()
+  #telnet_log_push()
+  time.sleep(30)
