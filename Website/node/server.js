61cccedc36031932461cb6fe4dc0b5d03536f43d
DevelopingHippo <developinghippo@gmail.com>
2023-10-13 01:50:10 -0400

    Designing Pies (Also rewrote the building the pies)

--- Website/node/server.js
+++ Website/node/server.js
@@ -116,7 +116,7 @@ app.get('/get-pie-data', cors(corsOptions), async function (req, res) {
         for (let i = 0; i < result.length - 1; i++) {
             formatted_result += '{"name": "' + result[i]['data'] + '", "share": ' + result[i]['total_count'] + '},';
         }
-        formatted_result += '{"name": "' + result[result.length - 1]['data'] + '", "share": ' + result[result.length - 1] + "}]";
+        formatted_result += '{"name": "' + result[result.length - 1]['data'] + '", "share": ' + result[result.length - 1]['total_count'] + "}]";
         let json_format = JSON.parse(formatted_result);
         res.json(json_format);
     }
