95f0266f0a77fb8a58e21285639cc6ac048d0e07
DevelopingHippo <developinghippo@gmail.com>
2023-10-14 21:33:25 -0400

    Designing Pies (Also rewrote the building the pies)

--- Website/apache/index.php
+++ Website/apache/index.php
@@ -6,15 +6,5 @@ if (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] == "false")
  }
 header("location: /auth/login.php");
 exit();
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>HoneyWall - Home</title>
-</head>
-<body>
-    <h1>This is the index page</h1>
-</body>
-</html>
+
