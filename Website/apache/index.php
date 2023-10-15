5430331c8bbf772b99169e030386402d3f9bb48d
DevelopingHippo <developinghippo@gmail.com>
2023-10-14 21:36:00 -0400

    Designing Pies (Also rewrote the building the pies)

--- Website/apache/index.php
+++ Website/apache/index.php
@@ -3,6 +3,15 @@ session_start();
 if (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] == "false")
  {
      $_SESSION["loggedIn"] = "false";
+     setcookie("pie-chart-1-selection", "username");
+     setcookie("pie-chart-2-selection", "password");
+     setcookie("pie-chart-3-selection", "src_ip");
+     setcookie("pie-chart-4-selection", "location");
+
+     setcookie("vert-1-selection", "username");
+     setcookie("vert-2-selection", "password");
+     setcookie("vert-3-selection", "src_ip");
+     setcookie("vert-4-selection", "location");
  }
 header("location: /auth/login.php");
 exit();
